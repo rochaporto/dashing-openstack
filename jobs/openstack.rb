d6a2d8501728f6ba7e0622f205ffe22eecd683f5
Ricardo Rocha <rocha.porto@gmail.com>
2014-04-04 11:43:03 +1300

    minimal config for aviator to work in openstack job.

--- jobs/openstack.rb
+++ jobs/openstack.rb
@@ -6,15 +6,20 @@ SCHEDULER.every '2s' do
   dashing_config = './config.yaml'
   config = YAML.load_file(dashing_config)
 
+  # handy for test envs, remove if not
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
   session = Aviator::Session.new(
-    config_file: 'aviator.yml',
-    environment: :production,
+    config_file: 'config.yaml',
+    environment: :aviator,
     log_file:    'aviator.log'
   )
-  session.authenticate
- 
+  session.authenticate do |params|
+    params.username    = 'admin'
+    params.password    = '123456'
+    params.tenant_name = 'openstack'
+  end
+
   keystone = session.identity_service
   response = keystone.request(:list_tenants)
   puts response.body
